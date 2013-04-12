e4bcc3c745a96b360a0085afa9ed4d518aae9098
Mike <Mike@Mikes-iMac.local>
2013-04-12 10:37:30 +0800

    branch test

--- GitSourceControlTest/GitSourceControlTestViewController.m
+++ GitSourceControlTest/GitSourceControlTestViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Git test");
+    NSLog(@"Git Branch test");
 }
 
 - (void)didReceiveMemoryWarning
